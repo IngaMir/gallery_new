ee10334e23125ed034214bb07fb584904eced970
IngaMir <InySechka2008@list.ru>
2022-06-23 19:15:15 +0300

    added model Card

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,8 @@ from web.views import index, detail, about
 
 app_name = "web"
 urlpatterns = [
-    path('', index),
-    path('detail/<int:pk>', detail),
+    path('', index, name='home'),
+    path('detail/<int:pk>', detail, name='detail'),
+    path('about/', about, name='about'),
+
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-def index(request):
-    return(request, 'index.html')
+from web.models import Card, Comment
 
-def detail(request):
-    return(request, 'detail.html')
+
+def index(request):
+    cards = Card.objects.all()
+
+    return render(request, 'index.html', {'cards': cards})
+
+
+def detail(request, pk):
+    card = Card.objects.get(id=pk)
+    comments = Comment.objects.filter(card_id=pk)
+    print(comments.query)
+    context = {'card': card, 'comments': comments}
+    return render(request, 'detail.html', context=context)
+
+def about(request):
+    return render(request, 'about.html')
